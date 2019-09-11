1ff59700d5ff1cba183d7f81930e97962b065ead
adisak <adisak.s.im@gmail.com>
2019-09-11 13:59:43 +0700

    just sample django app

---
+++ helloworld/hello/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+class SampleTest(TestCase):
+
+    def test_alway_pass(self):
+        self.assertEqual(1, 1)
+    
+    # def test_alway_fail(self):
+    #     self.assertEqual(2, 1)
